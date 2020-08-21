66e4f85d9e71bdb55f25dec7c61c00df1be269c7
Trevor Ewen <tewen@Trevors-MacBook-Pro-3.local>
2020-08-21 07:34:43 -0400

    cleanUrl() and cleanText() methods.

--- src/lib/url.spec.ts
+++ src/lib/url.spec.ts
@@ -1,7 +1,8 @@
 // tslint:disable:no-expression-statement
 import test from 'ava';
-import { domainAndSuffixFromUrl } from './url';
+import { cleanUrl, domainAndSuffixFromUrl } from './url';
 
+// domainAndSuffixFromUrl
 test('returns a blank string when passed undefined', t => {
   t.is(domainAndSuffixFromUrl(undefined), '');
 });
@@ -57,4 +58,73 @@ test('returns the domain and suffix when passed an http://wwww subdomain and suf
 
 test('returns the domain and suffix when passed a subdomain and suffix', t => {
   t.is(domainAndSuffixFromUrl('admin.tunaipsum.com'), 'admin.tunaipsum.com');
+});
+
+// cleanUrl
+test('returns undefined when passed undefined', t => {
+  t.is(cleanUrl(undefined), undefined);
+});
+
+test('returns undefined when passed null', t => {
+  t.is(cleanUrl(null), undefined);
+});
+
+test('returns undefined when passed blank', t => {
+  t.is(cleanUrl(''), undefined);
+});
+
+test('returns undefined when passed an invalid url string', t => {
+  t.is(cleanUrl('invalid url'), undefined);
+});
+
+test('returns a url with http when passed one without http', t => {
+  t.is(cleanUrl('motorhead.org'), 'http://motorhead.org');
+});
+
+test('returns a url with http when passed one with http', t => {
+  t.is(cleanUrl('http://koolaid.net'), 'http://koolaid.net');
+});
+
+test('returns a url with https when passed one with https', t => {
+  t.is(cleanUrl('https://chase.com'), 'https://chase.com');
+});
+
+test('plays nice with subdomains', t => {
+  t.is(cleanUrl('https://money.chase.com'), 'https://money.chase.com');
+});
+
+test('plays nice with www', t => {
+  t.is(cleanUrl('www.skittles.com'), 'http://www.skittles.com');
+});
+
+test('plays nice with end slashes', t => {
+  t.is(cleanUrl('www.skittles.com/'), 'http://www.skittles.com/');
+});
+
+test('plays nice with paths', t => {
+  t.is(
+    cleanUrl('www.skittles.com/path/to/place-there'),
+    'http://www.skittles.com/path/to/place-there'
+  );
+});
+
+test('plays nice with extensions', t => {
+  t.is(
+    cleanUrl('https://money.chase.com/agreement.txt'),
+    'https://money.chase.com/agreement.txt'
+  );
+  t.is(cleanUrl('chase.com/agreement.txt'), 'http://chase.com/agreement.txt');
+});
+
+test('plays nice with queries', t => {
+  t.is(
+    cleanUrl(
+      'https://money.chase.com/agreement.txt?success=true&failure=never'
+    ),
+    'https://money.chase.com/agreement.txt?success=true&failure=never'
+  );
+  t.is(
+    cleanUrl('chase.com?success=true&failure=never'),
+    'http://chase.com?success=true&failure=never'
+  );
 });
